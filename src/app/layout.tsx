debd44cef8d6b61f0f6f57e81be1024ffd373e94
yoursnawaz <shahnawaz162006@gmail.com>
2024-06-24 02:57:26 +0530

    feat: added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,32 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Shah Nawaz',
-  description: 'Full Stack Developer',
+  description: 'Developer | Designer | Researcher',
+  metadataBase: new URL('https://www.msnawaz.com'),
+  keywords: ['Shah Nawaz', 'Nawaz', 'Next.js', 'React.js', 'JavaScript', 'TypeScript'],
+  authors: [{ name: 'Shah Nawaz', url: 'https://www.msnawaz.com' }],
+  creator: 'Shah Nawaz',
+  publisher: 'Shah Nawaz',
+  openGraph: {
+    title: 'Shah Nawaz',
+    description: 'Developer | Designer | Researcher',
+    url: 'https://www.msnawaz.com',
+    siteName: 'Nawaz',
+    authors: ['Shah Nawaz'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
